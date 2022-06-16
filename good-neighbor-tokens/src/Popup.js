26f3bbd4d8f1f76d9d5ec0317a63d9e9ae08abd4
CharlotteW923 <CharlotteW923@gmail.com>
2022-06-16 00:06:59 -0400

    Added text box inside popup window

--- good-neighbor-tokens/src/Popup.js
+++ good-neighbor-tokens/src/Popup.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Popup = props => {
+    const[data,setData] = useState(null);
+    const [print,setPrint]=useState(false);
+    function getData(val){
+        setData(val.target.value)
+        setPrint(false)
+        console.warn(val.target.value)
+    }
+
     return (
         <div className="popup-box">
             <div className="box">
                 <span className="close-icon" onClick={props.handleClose}>x</span>
                 {props.content}
+                <div className = "TextBox">{
+                    print?
+                    <h2>Message sent</h2>
+                    :null
+                }
+                <input type = "text" onChange={getData}/>
+                <button onClick={()=>setPrint(true)}>Send</button>
+                </div>
             </div>
         </div>
     );
